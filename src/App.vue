1ded9ae505da1c4b2cced407833b59d0d75a61a4
yji35j42012 <35093568+yji35j42012@users.noreply.github.com>
2021-06-24 09:29:32 +0800

    檔案同步

--- src/App.vue
+++ src/App.vue
@@ -1,13 +1,48 @@
 <template>
-	<header>
-		
-	</header>
-    <HelloWorld msg="Welcome to Your Vue.js App" />
+    <header>
+        <div class="member_icon"></div>
+        <ul class="nav">
+            <li></li>
+        </ul>
+    </header>
+    <div class="container">
+        <div class="function">
+            <button type="button" class="btn _primary">按鈕</button
+            ><label class="normal_search"
+                ><input type="text" /><i class="material-icons">
+                    search
+                </i></label
+            >
+        </div>
+        <div class="tbBox">
+            <div class="tbBox_head">
+                <table>
+                    <caption class="tbBox_title"></caption>
+                    <tr>
+                        <th class="_tb_w20%">欄位1</th>
+                        <th class="_tb_w20%">欄位2</th>
+                        <th class="_tb_w20%">欄位3</th>
+                        <th class="_tb_w20%">欄位4</th>
+                        <th class="_tb_w20%">欄位5</th>
+                    </tr>
+                </table>
+            </div>
+            <div class="tbBox_body">
+                <!-- <TitleTable
+                v-for="(item, index) in member1.sort"
+                :key="index"
+                :stockNmae="item"
+                :stockIndex="index"
+            ></TitleTable> -->
+            </div>
+        </div>
+        <!--v-if-->
+    </div>
+    <!-- <HelloWorld msg="Welcome to Your Vue.js App" /> -->
 </template>
 
 <script>
 import HelloWorld from "./components/HelloWorld.vue";
-<style lang="scss" src="./assets/scss/all.scss"></style>;
 export default {
     name: "App",
     components: {
@@ -15,14 +50,7 @@ export default {
     },
 };
 </script>
+<style lang="scss" src="./assets/scss/all.scss"></style>
 
 <style lang="scss">
-#app {
-    font-family: Avenir, Helvetica, Arial, sans-serif;
-    -webkit-font-smoothing: antialiased;
-    -moz-osx-font-smoothing: grayscale;
-    text-align: center;
-    color: #2c3e50;
-    margin-top: 60px;
-}
 </style>
